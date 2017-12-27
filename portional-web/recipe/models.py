fad1dfeed979fce45e1b5dbc3f64479144a62ddb
David House <david_house@harvard.edu>
2017-12-26 20:43:31 -0700

    feat: Implement initial database models

--- portional-web/recipe/models.py
+++ portional-web/recipe/models.py
@@ -1,3 +1,65 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class Recipe(UUIDModel):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    imported_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Ingredient(UUIDModel):
+    orig_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f'{self.orig_name}'
+
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient,
+                                   on_delete=models.CASCADE,
+                                   related_name='uses')
+    amount = models.FloatField()
+    scale = models.CharField(max_length=20, blank=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.amount} {self.scale} {self.ingredient}'
+
+    def __repr__(self):
+        return f'Recipe Ingredient: {self} used in {self.recipe}'
+
+
+class Instruction(models.Model):
+    orig_text = models.TextField()
+    db_id_text = models.TextField(blank=True)
+    order = models.SmallIntegerField()
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(RecipeIngredient, blank=True)
+
+    def compile(self, prefix='', suffix=''):
+        if not self.ingredients.all():
+            return  self.orig_text  # No compilation required
+        ingredient_id_map = {
+            ing.id: f'{prefix}{str(ing)}{suffix}'
+            for ing in self.ingredients.all()
+        }
+        self.db_id_text = self.db_id_text.format(**ingredient_id_map)
+        return self.db_id_text
+
+    def __str__(self):
+        return f'{self.recipe} {self.order}: {self.orig_text}'
+
+
+class Author(models.Model):
+    pass
